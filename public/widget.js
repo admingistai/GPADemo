163ee39e9c30ccda2cae7860a499dfd80ef7b044
cjprorata <christopher@prorata.ai>
2025-06-17 08:43:59 -0700

    Update widget.js

--- public/widget.js
+++ public/widget.js
@@ -1064,9 +1064,9 @@
                     pointer-events: none;
                     text-align: left;
                     color: #6b7280;
-                    margin-left: 28px;
+                    margin-left: 20px;
                     transition: all 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94);
-                    width: calc(100% - 50px);
+                    width: calc(100% - 42px);
                     min-width: 100px;
                 }
                 
