52eb712a391e564427870855b09c6a0aca87ec35
cjprorata <christopher@prorata.ai>
2025-06-18 17:44:27 -0700

    favicon

--- pages/index.js
+++ pages/index.js
@@ -121,8 +121,10 @@ export default function Home() {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-        <link rel="apple-touch-icon" href="/favicon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon-192x192.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/favicon-192x192.png" />
         <script src="/widget.js" async></script>
       </Head>
       
