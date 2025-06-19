f4f5ecbf2b3b633b1f27e3a084d98426ad8279b7
cjprorata <christopher@prorata.ai>
2025-06-18 17:30:10 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -364,10 +364,10 @@ export default function Home() {
           flex: 1;
           display: flex;
           flex-direction: column;
-          align-items: center;
+          align-items: flex-start;
           justify-content: center;
           padding: 1rem 2rem;
-          text-align: center;
+          text-align: left;
         }
 
         .main-title {
